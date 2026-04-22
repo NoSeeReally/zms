ce1dd30f7f048acd2e417f52e1334b15fe2726ee
Angel Nocerale <22-1-01120@vsu.edu.ph>
2026-04-22 20:25:17 +0800

    Add cookie component

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,29 @@ import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import Lesson from "./pages/Lesson";
 import Profile from "./pages/Profile";
-
+import Cookies from "./components/Cookies";
+import { useState } from "react";
 
 export default function App(){
+    const [isVisible, setIsVisible] = useState<boolean>(true)
+
+    const cookieSetter = () => {
+        setIsVisible(false)
+    }
+    const cookieHandler = () => {
+        if(isVisible)
+        {
+            return(
+                <Cookies setState={cookieSetter}/>
+            );
+        }
+    }
     return(
        <BrowserRouter> 
             <div className="header">
                 <Header />
                 <NavBar />
             </div>
-           
-
             
             <Routes>
                 <Route path="/home" element={<Home/>} />
@@ -30,8 +42,13 @@ export default function App(){
                 <Route path="lessons" element={<Lesson/>}/>
                 <Route path="profile" element={<Profile/>}/>
             </Routes>
+             <div className="body">
 
-            
+           </div>
+
+            <div className="padding">
+             {cookieHandler()}
+           </div>
             <Copyright />
         </BrowserRouter>
     );
---
+++ src/components/Cookies.tsx
@@ -0,0 +1,32 @@
+import '../App.css'
+import { useNavigate } from 'react-router-dom';
+interface CookieProp{
+    setState:() => void;
+}
+export default function Cookies({setState}: CookieProp){
+    const navigate = useNavigate();
+
+    const termsHandler = () =>{
+        setState();
+        navigate('/terms');
+    }
+    
+    return(
+        <>
+        <div className=''>
+            <div className='cookie-card'>
+                <div className='cookie-card-title'>
+                    <p>Cookies</p>
+                    <p>We use cookies for a better experience.</p>
+                    <p>Click Read More to know more</p>
+                    <button className='cookie-card-button' onClick={setState}>Agree</button>
+                    <button className='cookie-card-button' onClick={termsHandler}>Read More</button>
+
+                </div>
+
+            </div>
+        </div>
+
+        </>
+    );
+}
