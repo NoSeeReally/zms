0ed4149d2aa2c63836054d5e76a3e8ca59a88012
Angel Nocerale <22-1-01120@vsu.edu.ph>
2026-06-03 23:51:02 +0800

    feat: wire button to their respective lesson component

--- src/components/LessonCard.tsx
+++ src/components/LessonCard.tsx
@@ -1,17 +1,14 @@
+import { Link } from 'react-router-dom';
 interface LessonCardProp{
     title:string
-    status?: 'ongoing' | 'Yet to learn'
 }
 
-export default function LessonCard({title, status = 'Yet to learn'}:LessonCardProp){
-    const statusClassName = status === 'ongoing' ? 'lesson-pill ongoing' : 'lesson-pill yet-to-learn';
-
+export default function LessonCard({title}:LessonCardProp){
     return(
         <div className='lesson-card'>
             <h3 className='lesson-card-title'>{title}</h3>
             <div className='lesson-card-actions'>
-                <span className={statusClassName}>{status}</span>
-                <button className='lesson-card-button' type='button'>Open Lesson</button>
+                <Link className='lesson-card-button' to={`/${title}`}>Open Lesson</Link>
             </div>
         </div>
     );
