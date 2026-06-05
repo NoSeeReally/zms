459d0939333607ead87173251e97dec9b7c0b44d
Angel Nocerale <22-1-01120@vsu.edu.ph>
2026-06-06 00:37:05 +0800

    Feat: Route sublesson card to respective page

--- src/components/SubLessonCard.tsx
+++ src/components/SubLessonCard.tsx
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+
 interface SubLessonsProp{
     title:string
 }
 export default function SubLessonCard({title}:SubLessonsProp){
     return(
         <>
-            <div className="subLesson-card">
+            <Link className="subLesson-card" to={`/${title}`} aria-label={`Open ${title}`}>
                 <h4 className="subLesson-card-title">{title}</h4>
-            </div>
+            </Link>
         </>
     );
 }
