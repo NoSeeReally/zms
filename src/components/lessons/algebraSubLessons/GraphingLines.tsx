f9eded0be5ea7f9992199078aa4679f327385b2b
Angel Nocerale <22-1-01120@vsu.edu.ph>
2026-06-07 02:05:05 +0800

    Add: sublessons for algebra to initialize page routing

---
+++ src/components/lessons/algebraSubLessons/GraphingLines.tsx
@@ -0,0 +1,8 @@
+export default function GraphingLines(){
+
+    return(
+        <>
+        
+        </>
+    );
+}
---
+++ src/components/lessons/algebraSubLessons/Quadratics.tsx
@@ -0,0 +1,8 @@
+export default function Quadratics(){
+
+    return(
+        <>
+        
+        </>
+    );
+}
